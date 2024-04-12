fd9b82238310bd15abfc76b2177e0f3f536499b8
cwhite723 <c_white723@naver.com>
2024-04-12 20:13:41 +0900

    회원 관리 예제 실습

---
+++ src/main/java/hayan/springbasic/service/MemberService.java
@@ -0,0 +1,42 @@
+package hayan.springbasic.service;
+
+import hayan.springbasic.domain.Member;
+import hayan.springbasic.repository.MemberRepository;
+import hayan.springbasic.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // id로 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
